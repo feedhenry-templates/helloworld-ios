e6efc77dc7a0d8e40bd9127b9e060d0d8fa19497
corinne krych <corinnekrych@gmail.com>
2015-09-04 11:49:51 +0200

    init prior to any cloud call

--- iOS-Template-App/HomeViewController.m
+++ iOS-Template-App/HomeViewController.m
@@ -22,18 +22,25 @@
 
 
 - (IBAction)cloudCall:(id)sender {
-  NSDictionary *args = [NSDictionary dictionaryWithObject:name.text forKey:@"hello"];
-  FHCloudRequest *req = (FHCloudRequest *) [FH buildCloudRequest:@"/hello" WithMethod:@"POST" AndHeaders:nil AndArgs:args];
-  
-  [req execAsyncWithSuccess:^(FHResponse * res) {
-    // Response
-    NSLog(@"Response: %@", res.rawResponseAsString);
-    result.text = [res.parsedResponse objectForKey:@"msg"];
-  } AndFailure:^(FHResponse * res){
-    // Errors
-    NSLog(@"Failed to call. Response = %@", res.rawResponseAsString);
-    result.text = res.rawResponseAsString;
-  }];
+    [FH initWithSuccess:^(FHResponse *response) {
+        NSLog(@"initialized OK");
+        
+        NSDictionary *args = [NSDictionary dictionaryWithObject:name.text forKey:@"hello"];
+        FHCloudRequest *req = (FHCloudRequest *) [FH buildCloudRequest:@"/hello" WithMethod:@"POST" AndHeaders:nil AndArgs:args];
+        
+        [req execAsyncWithSuccess:^(FHResponse * res) {
+            // Response
+            NSLog(@"Response: %@", res.rawResponseAsString);
+            result.text = [res.parsedResponse objectForKey:@"msg"];
+        } AndFailure:^(FHResponse * res){
+            // Errors
+            NSLog(@"Failed to call. Response = %@", res.rawResponseAsString);
+            result.text = res.rawResponseAsString;
+        }];
+        
+    } AndFailure:^(FHResponse *response) {
+        NSLog(@"initialize fail, %@", response.rawResponseAsString);
+    }];
 }
 
 @end
