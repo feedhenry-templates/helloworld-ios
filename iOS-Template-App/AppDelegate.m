eb9488322cf0cb44d60d15f9e82308768af7ae6d
corinne krych <corinnekrych@gmail.com>
2015-11-20 11:02:02 +0100

    remove unused code

--- iOS-Template-App/AppDelegate.m
+++ iOS-Template-App/AppDelegate.m
@@ -19,11 +19,6 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [FH initWithSuccess:^(FHResponse *response) {
-    NSLog(@"initialized OK");
-  } AndFailure:^(FHResponse *response) {
-    NSLog(@"initialize fail, %@", response.rawResponseAsString);
-  }];
   return YES;
 }
 
